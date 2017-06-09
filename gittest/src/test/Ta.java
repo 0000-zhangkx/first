153e59226f6e0253304af6b203e853ec1374147c
Administrator <Administrator@A-201702151145>
2017-06-09 11:30:38 +0800

    推送到github

--- gittest/src/test/Ta.java
+++ gittest/src/test/Ta.java
@@ -4,7 +4,10 @@ public class Ta {
 	public int sum(int x,int y){
 		return x+y;
 	}
+	public void print(){
+		
+	}
 	public static void main(String[] args){
-		//TODO Auto-generated method stub
+		Ta aTa=new Ta();
 	}
 }
