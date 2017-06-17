11c7d3c1ade0b913c383de95d30b3ce5f11d896c
0000-zhangkx <592557982@qq.com>
2017-06-17 11:12:24 +0800

    上传

--- gittest/src/test/Ta.java
+++ gittest/src/test/Ta.java
@@ -4,10 +4,11 @@ public class Ta {
 	public int sum(int x,int y){
 		return x+y;
 	}
-	public void print(){
-		
+	public void print(int z){
+		System.out.println(z);
 	}
 	public static void main(String[] args){
 		Ta aTa=new Ta();
+		aTa.print(3);
 	}
 }
